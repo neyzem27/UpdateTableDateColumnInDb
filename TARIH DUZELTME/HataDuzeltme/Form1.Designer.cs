2a09ae6e6d4ab17e0cb6fc9172d26ac11525b4c8
neyzem27 <129578415+neyzem27@users.noreply.github.com>
2023-04-17 10:06:31 +0300

    Add files via upload

---
+++ TARIH DUZELTME/HataDuzeltme/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace HataDuzeltme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.txtSira = new System.Windows.Forms.TextBox();
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSeri = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(365, 39);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "İşlemi Başlat";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // txtSira
+            // 
+            this.txtSira.Location = new System.Drawing.Point(236, 8);
+            this.txtSira.Name = "txtSira";
+            this.txtSira.Size = new System.Drawing.Size(93, 20);
+            this.txtSira.TabIndex = 1;
+            // 
+            // OFD
+            // 
+            this.OFD.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(194, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SIRA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(9, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SERİ";
+            // 
+            // txtSeri
+            // 
+            this.txtSeri.Location = new System.Drawing.Point(63, 9);
+            this.txtSeri.Name = "txtSeri";
+            this.txtSeri.Size = new System.Drawing.Size(100, 20);
+            this.txtSeri.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(9, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tarih";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(63, 40);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 72);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSeri);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSira);
+            this.Controls.Add(this.btnImport);
+            this.Name = "Form1";
+            this.Text = "Tarih Düzeltme";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.TextBox txtSira;
+        private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSeri;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ TARIH DUZELTME/HataDuzeltme/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Xml;
+
+namespace HataDuzeltme
+{
+    public partial class Form1 : Form
+    {
+        // Getting connection string from App.config file
+        string StrCon = ConfigurationManager.ConnectionStrings["hedefDb"].ToString();//silinecek db
+        string anaDB = ConfigurationManager.ConnectionStrings["anaDB"].ToString();//ana db
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            // string XMlFile = txtFilePath.Text;
+            try
+            {
+                if (!txtSeri.Text.Equals("") && !txtSira.Text.Equals(""))
+                {
+                    /* // Conversion Xml file to DataTable
+                     DataTable dt = CreateDataTableXML(XMlFile);
+                     if (dt.Columns.Count == 0)
+                         dt.ReadXml(XMlFile);
+                    */
+                    // Creating Query for Table Creation
+                    //string Query = CreateTableQuery(dt);  ----@"Data Source=192.168.1.219\SQLEXPJUMP;Initial Catalog=MikroDB_V16_004;User ID =sa;Password =Sa1234;Integrated Security=True"
+                    SqlConnection con = new SqlConnection(StrCon);//
+                    con.Open();
+
+                    // Deletion of Table 2022-10-22 00:00:00.000
+                    SqlCommand cmd = new SqlCommand("UPDATE " + anaDB + ".[dbo].[CARI_HESAP_HAREKETLERI] SET cha_belge_tarih='" + dateTimePicker1.Text+ "',cha_tarihi='" + dateTimePicker1.Text + "' where cha_evrak_tip=63 and cha_evrakno_seri='" + txtSeri.Text.ToUpper() + "' and cha_evrakno_sira in (" + txtSira.Text + ");", con);
+                    int check = cmd.ExecuteNonQuery();
+
+                    if (check != 0)
+                    {
+                        // Copy Data from DataTable to Sql Table
+
+                        txtSeri.Text = "";
+                        txtSira.Text = "";
+                        MessageBox.Show("işlem başarıyla yapılmıştır.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data düzeltmesi yapılmadı.2 sebepten olur.1)db ayarları 2)yanlış kod girilmesi");
+                    }
+                    con.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen seri ve sıra alanlarını doldurun.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("İşlem başarısız.Hata: " + ex);
+            }
+
+        }
+    }
+}
